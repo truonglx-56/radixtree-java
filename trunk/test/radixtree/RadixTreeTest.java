7d28cb3ef97de9b868a0d556d02d8db067a0e15a
dave.dorasinger@gmail.com <dave.dorasinger@gmail.com@48280b40-90c4-7fb6-4e90-8e6a9fb11cf5>
2011-05-04 02:34:55 +0000

    Added licensing text

--- trunk/test/radixtree/RadixTreeTest.java
+++ trunk/test/radixtree/RadixTreeTest.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package radixtree;
 
@@ -18,8 +14,18 @@ import static org.junit.Assert.*;
 
 /**
  *
- * @author daves
+ * @author David Singer
+ * This code is not licensed and may be used in any way.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
  */
+ 
 public class RadixTreeTest {
 
     public RadixTreeTest() {
